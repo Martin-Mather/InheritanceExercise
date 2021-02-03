12d3fdfe96a817c1121759a99bdaef57905a4afb
Martin Mather <hondaccord96@gmail.com>
2021-02-03 12:28:46 -0700

    finished

---
+++ Inheritance/Bird.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Bird : Animal
+    {
+        public Bird()
+        {
+
+        }
+        public int Wings { get; set; }
+        public string Type { get; set; }
+        public bool IsWarmBlooded { get; set; }
+        public bool HasFeathers { get; set; }
+        
+        //public Bird(string name, int age, int legs, bool hasBackbone, bool isWarmblooded, bool hasFeathers, bool HasFeathers);
+          //{
+        
+            //Name = name;
+            //Age = Age;
+            //Legs = legs;
+            //HasBackbone = hasBackbone;
+            //IsWarmBlooded = isWarmBlooded;
+            //HasFeathers = hasFeathers;
+            //Wings = wings;
+            //Type = type;
+          //}
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -8,12 +8,12 @@ namespace Inheritance
         {
             // TODO Be sure to follow best practice when creating your classes
 
-            // Create a class Animal
+            // Create a class Animal -Done
             // give this class 4 members that all Animals have in common
 
 
-            // Create a class Bird
-            // give this class 4 members that are specific to Bird
+            // Create a class Bird -done
+            // give this class 4 members that are specific to Bird - done
             // Set this class to inherit from your Animal Class
 
             // Create a class Reptile
@@ -28,12 +28,43 @@ namespace Inheritance
              *  
              * Creatively display the class member values 
              */
+            Bird zapdos = new Bird()
+            {
+                Name = "Zapdos",
+                Age = 1000,
+                Legs = 2,
+                HasBackbone = true,
+                HasFeathers = true,
+                Wings = 2,
+                Type = "electric",
 
+            };
+            Console.WriteLine($"{zapdos.Name} is an {zapdos.Type} pokemon that is {zapdos.Age} years old and has"+
+                $"{zapdos.Legs} legs and {zapdos.Wings} wings" );
+            Console.WriteLine($"Is {zapdos.Name} a vertebrate, warm blooded and has feathers?"+
+                $"{zapdos.HasBackbone}, {zapdos.IsWarmBlooded}, and {zapdos.HasFeathers}");
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Bird class
              *  
              * Creatively display the class member values 
              */
+            Reptile dragon = new Reptile()
+            {
+                Name = "Smaug",
+                Age = 1200,
+                Legs = 4,
+                HasBackbone = true,
+                IsColdBlooded = true,
+                HasScales = true,
+                Environment = "Erabor",
+                Defense = "Breathes Fire"
+
+            };
+            Console.WriteLine($"{dragon.Name} is a {dragon.Age} year old dragon that lives under {dragon.Environment}" +
+                $"he has {dragon.Legs} legs and {dragon.Defense}.");
+            Console.WriteLine($"Is {dragon.Name} a vertebrate, cold blooded and has scales?" +
+                $"{dragon.HasBackbone}, {dragon.IsColdBlooded}, and {dragon.HasScales}");
+            Console.WriteLine();
         }
     }
 }
